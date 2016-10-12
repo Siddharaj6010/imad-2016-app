69c1d5e0503cf62d8f21cd987cc3821a9823aeb8
Siddharaj Solanki <Siddharaj6010@gmail.com>
2016-10-13 00:07:30 +0530

    Create dance.js

---
+++ ui/dance.js
@@ -0,0 +1,32 @@
+console.log('Loaded!');
+
+
+// Move the image
+var img = document.getElementById('img');
+var stop = document.getElementById('stop');
+
+var marginLeft = 0;
+var direction = 1;
+var w = document.getElementById('container').offsetWidth-40;
+var interval = [];
+
+function moveRightLeft() {
+    if(marginLeft == w-img.width ) {
+        direction = -1;
+    } 
+    else if(marginLeft == -(w-img.width) ) {
+        direction = 1;
+    }
+    marginLeft = marginLeft + direction;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.parentElement.onclick = function() {
+    interval.push(setInterval(moveRightLeft, 10));
+};
+
+stop.onclick = function() {
+    for(var i=0; i<interval.length; i++){
+        clearInterval(interval[i]);   
+    }
+    //alert(interval.length);
+};
