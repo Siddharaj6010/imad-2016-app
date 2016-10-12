a7f24ebab791c1a91ba1f2b60ff4e09de9883146
Siddharaj Solanki <Siddharaj6010@gmail.com>
2016-10-12 20:58:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,9 +10,9 @@ element.innerHTML = 'New Value';
 var img = document.getElementById('img');
 var marginLeft = 0;
 function moveRight() {
-    marginLeft = marginLeft + 5;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function() {
-    var interval = setInterval(moveRight, 50);
+    var interval = setInterval(moveRight, 10);
 };
