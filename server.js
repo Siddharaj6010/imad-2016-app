dac21dcb72dffb9f436e67c8e4b008a084618c64
Siddharaj Solanki <Siddharaj6010@gmail.com>
2016-10-12 23:58:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,6 +79,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/dance', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'dance.html'));
+});
+
+
+
 app.get('/:articleName', function (req, res) {
     // articleName == article-one
     // articles[articleName] == {} content object for article one
