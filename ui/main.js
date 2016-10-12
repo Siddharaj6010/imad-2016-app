a6d82a7861322741ebf3eeb009c5c0df94951d36
Siddharaj Solanki <Siddharaj6010@gmail.com>
2016-10-13 02:21:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -68,7 +68,7 @@ submit.onclick = function () {
     // send request to the server
     var nameInput = document.getElementById('name');
     var name = nameInput.value;    
-    request.open('GET','http://siddharaj6010.imad.hasura-app.io/submit-name/' + name,true);
+    request.open('GET','http://siddharaj6010.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
 };    
     
