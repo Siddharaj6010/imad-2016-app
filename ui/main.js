a7028f6a7ce79177ebf4ddd0ace6d9b6574b4405
Siddharaj Solanki <Siddharaj6010@gmail.com>
2016-10-13 01:37:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,3 +39,39 @@ button.onclick = function() {
     // Make the request
     request.open('GET','http://siddharaj6010.imad.hasura-app.io/counter-read',true);
     request.send(null);
+    
+// Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+    
+submit.onclick = function () {
+    // Make a request to the server and send the name
+    
+    // Capture a list of names and render it as a list
+    var names = ['name1','name2','name3'];
+    var list = '';
+    for(var i=0;i<names.length;i++) {
+        list += '<li>' + names[i] + '</li>';
+    }
+    
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
