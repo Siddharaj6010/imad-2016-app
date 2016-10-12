9640eecd05171153bf315d9688b21569035450a5
Siddharaj Solanki <Siddharaj6010@gmail.com>
2016-10-12 20:50:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+
+// Change the text of the main-text div
+
+var element = document.getElementById('main-text');
+
+element.innerHTML = 'New Value';
+
+// Move the image
+var img = document.getElementById('img');
+
+img.onClick = function() {
+    img.style.marginLeft = '100px';
+};
