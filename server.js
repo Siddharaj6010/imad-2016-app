537fc62ed14da27aca99aeab65bf378548ea6f4b
Siddharaj Solanki <Siddharaj6010@gmail.com>
2016-10-25 23:51:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-//var Pool = require('pg');
+var Pool = require('pg');
 
-/*var config = {
+var config = {
     user: 'siddharaj6010',
     database: 'siddharaj6010',
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
 };
-*/
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -89,8 +89,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-//var pool = new Pool(config);
-/*app.get('/test-db', function (req, res){
+var pool = new Pool(config);
+app.get('/test-db', function (req, res){
     //make a select request
     //return a response with results
     pool.query("SELECT * FROM test", function(err, result){
@@ -101,7 +101,7 @@ app.get('/', function (req, res) {
         }
     } );
 });
-*/
+
 var counter = 0;
 app.get('/counter', function(req, res){
     counter = counter + 1;
