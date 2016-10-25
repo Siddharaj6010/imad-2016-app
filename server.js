301ada774fedf3bcef3c6116c47128e41982f0de
Siddharaj Solanki <Siddharaj6010@gmail.com>
2016-10-26 00:10:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.get('/', function (req, res) {
 });
 
 var pool = new Pool(config);
-app.get('/test-db', function (req, res){
+/*app.get('/test-db', function (req, res){
     //make a select request
     //return a response with results
     pool.query("SELECT * FROM test", function(err, result){
@@ -101,6 +101,7 @@ app.get('/test-db', function (req, res){
         }
     } );
 });
+*/
 
 var counter = 0;
 app.get('/counter', function(req, res){
